b1623be9666de114ca3d4cf5ab3fcf1e2784d1d7
Suleman Elahi <sulemane010@gmail.com>
2022-04-19 06:59:57 +0500

    removed unused image and update the text

--- class2/src/App.js
+++ class2/src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,7 +13,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Suleman
         </a>
       </header>
     </div>
